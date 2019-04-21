f43ea3550e2f966c74e0cf940ced4db7a3a96210
Drew Logsdon <dalogsdon@gmail.com>
2019-04-20 23:09:06 -0500

    initial publish

---
+++ precache-manifest.5adf66dcb646d581b97718925c41d5c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c96e90947479d05374fe",
+    "url": "/recipes/static/js/runtime~main.19e64e71.js"
+  },
+  {
+    "revision": "d2aa043db44b26629f53",
+    "url": "/recipes/static/js/main.7070bb00.chunk.js"
+  },
+  {
+    "revision": "9a65c7b9c3d9ff4ea124",
+    "url": "/recipes/static/js/2.d334b9f1.chunk.js"
+  },
+  {
+    "revision": "d2aa043db44b26629f53",
+    "url": "/recipes/static/css/main.4bdf1e6f.chunk.css"
+  },
+  {
+    "revision": "9b0b4204ae95cb175a573999216ec1ec",
+    "url": "/recipes/index.html"
+  }
+];
