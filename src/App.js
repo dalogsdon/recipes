a9e1bfbf75e640ae360b7c0455f42db3512c7410
Drew Logsdon <dalogsdon@gmail.com>
2019-04-20 23:25:06 -0500

    Use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Recipe from './Recipe';
 import recipes from './recipes';
 import './App.css';
